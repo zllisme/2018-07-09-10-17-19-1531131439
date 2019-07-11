7be763f93851a5b608bc3e53516dc4b426c7d0a4
zllisme <1973842936@qq.com>
2019-07-11 22:39:30 +0800

    feat: finish pratice 4 and 5

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,13 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person{
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("%s I am a Worker. I have a job.", super.introduce());
+    }
 }
+
--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -1,4 +1,12 @@
 package practice05;
 
-public class Worker {
+public class Worker extends Person{
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("%s I am a Worker. I have a job.", super.introduce());
+    }
 }
