30c774a32455cc7d2a755fbd1b42a495ce4622b2
zllisme <1973842936@qq.com>
2019-07-11 22:54:35 +0800

    feat: finish practice 6

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,29 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int Klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        Klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return Klass;
+    }
+
+    public void setKlass(int klass) {
+        Klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return this.Klass != 0 ?
+                String.format("%s I am a Teacher. I teach Class %d.", super.introduce(), this.Klass)
+                : String.format("%s I am a Teacher. I teach No Class.", super.introduce());
+    }
 }
