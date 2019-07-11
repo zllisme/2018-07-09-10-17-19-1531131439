09d728ef8a223d9aff4874127823c66f0e182213
zllisme <1973842936@qq.com>
2019-07-11 22:22:04 +0800

    feat: finish practice 2 and 3

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,23 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+    private int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("I am a Student. I am at Class %d.", this.klass);
+    }
 }
